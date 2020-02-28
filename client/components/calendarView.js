d98fd30790cd3b2c0e49921155991206e7cdbc62
Ian Gallagher <igallag93@gmail.com>
2020-02-28 18:42:21 -0500

    added a few lines to the new component

---
+++ client/components/calendarView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div>
+      <h1>This is just a test</h1>
+      <p>
+        Going to test calendars and abilty to schedule and notify the propper
+        people
+      </p>
+    </div>
+  )
+}
+
+export default Calendar
